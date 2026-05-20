12d781e8fe56ca872cbfe9e147b77e7aa351c7a4
VictorRiveraT <victor.rivera@upch.pe>
2026-05-19 22:55:13 -0500

    feat(FEAT01): agrega portal de consulta publica con UI de Vanesa

--- services/ticket-service/src/index.js
+++ services/ticket-service/src/index.js
@@ -3,11 +3,15 @@ const express = require("express");
 const cors = require("cors");
 const ticketRoutes = require("./routes/ticket.routes");
 
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Health check
 app.get("/health", (req, res) => {
